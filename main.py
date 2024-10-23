d7aef6dc0c5fc93563b2e73608680a7b3f48f964
aldkre <krebs.aldona@op.pl>
2024-10-23 20:57:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-
 st.set_page_config(
-    page_title="Konwerter",
+    page_title="Margin calc",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="collapsed",
     menu_items={}
 )
 
-#MARKDOWN STYLE
-st.markdown("""
-<style>
-.title {
-    font-size: 30px;
-    color: #A27C24;
-    font-weight: 900;
-    font-style: normal;
-    text-align: center;
-}
-</style>
-""", unsafe_allow_html=True)
-
-
 st.divider()
 
-def create_table_from_zakup_netto(zakup_netto, VAT, marza):
-    return ({'zakup netto': round(zakup_netto, 2),
-                'zakup brutto': round(zakup_netto * (1 + VAT), 2),
-                'VAT': round(VAT_display, 0),
-                'marża brutto': round(marza * 100, 0),
-                'zysk': round((zakup_netto*(1 + VAT)/(1 - marza)/(1 + VAT)) - zakup_netto, 2),
-                'cena brutto': round(zakup_netto*(1 + VAT) / (1 - marza), 2),
-                'cena netto': round((zakup_netto*(1 + VAT)/(1 - marza)) / (1 + VAT), 2)})
 
-
-def print_table_zakup_netto():
-    temp_data = create_table_from_zakup_netto(zakup_netto= st.session_state.zakup_netto_value, VAT=st.session_state.VAT/100, marza=st.session_state.marza_value/100)
+def table_formatting(temp_data):
     temp_df = pd.DataFrame.from_dict(data=temp_data, orient='index', columns=['Wartości']).transpose()
     st.data_editor(
         temp_df,
@@ -74,6 +49,22 @@ def print_table_zakup_netto():
         }
     )
 
+
+def create_table_from_zakup_netto(zakup_netto, VAT, marza):
+    return ({'zakup netto': round(zakup_netto, 2),
+                'zakup brutto': round(zakup_netto * (1 + VAT), 2),
+                'VAT': round(VAT_display, 0),
+                'marża brutto': round(marza * 100, 0),
+                'zysk': round((zakup_netto*(1 + VAT)/(1 - marza)/(1 + VAT)) - zakup_netto, 2),
+                'cena brutto': round(zakup_netto*(1 + VAT) / (1 - marza), 2),
+                'cena netto': round((zakup_netto*(1 + VAT)/(1 - marza)) / (1 + VAT), 2)})
+
+
+def print_table_from_zakup_netto():
+    temp_data = create_table_from_zakup_netto(zakup_netto= st.session_state.zakup_netto_value, VAT=st.session_state.VAT/100, marza=st.session_state.marza_value/100)
+    table_formatting(temp_data)
+
+
 def create_table_from_zakup_brutto(zakup_brutto, VAT, marza):
     return ({'zakup netto': round(zakup_brutto/(1 + VAT), 2),
                 'zakup brutto': round(zakup_brutto, 2),
@@ -83,43 +74,11 @@ def create_table_from_zakup_brutto(zakup_brutto, VAT, marza):
                 'cena brutto': round(zakup_brutto / (1 - marza), 2),
                 'cena netto': round(zakup_brutto/(1 - marza) / (1 + VAT), 2)})
 
-def print_table_zakup_brutto():
-    temp_dict = create_table_from_zakup_brutto(zakup_brutto= st.session_state.zakup_brutto_value, VAT=st.session_state.VAT/100, marza=st.session_state.marza_value/100)
-    temp_df = pd.DataFrame.from_dict(temp_dict, orient='index', columns=['Wartości']).transpose()
-    st.data_editor(
-        temp_df,
-        column_order=['zakup netto', 'zakup brutto', 'VAT', 'marża brutto', 'zysk', 'cena netto', 'cena brutto'],
-        column_config={
-            "zakup netto": st.column_config.NumberColumn(
-                "Cena zakupu netto",
-                help="Cena zakupu netto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "zakup brutto": st.column_config.NumberColumn(
-                "Cena zakupu brutto",
-                help="Cena zakupu brutto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "VAT": st.column_config.NumberColumn(
-                "Stawka VAT",
-                help="Stawka podatku VAT wyrażona procentowo",
-                format="%.2f %%"),
-            "marża brutto": st.column_config.NumberColumn(
-                "Stawka marży brutto",
-                help="Marża brutto wyrażona procentowo",
-                format="%.2f %%"),
-            "zysk": st.column_config.NumberColumn(
-                "Zysk netto",
-                help="Zysk wyrażony w złotówkach, stanowiący różnicę między ceną sprzedaży netto a ceną zakupu netto",
-                format="%.2f zł"),
-            "cena netto": st.column_config.NumberColumn(
-                "Cena sprzedaży netto",
-                help="Cena sprzedaży netto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "cena brutto": st.column_config.NumberColumn(
-                "Cena sprzedaży brutto",
-                help="Cena sprzedaży brutto wyrażona w złotówkach",
-                format="%.2f zł"),
-        }
-    )
+
+def print_table_from_zakup_brutto():
+    temp_data = create_table_from_zakup_brutto(zakup_brutto= st.session_state.zakup_brutto_value, VAT=st.session_state.VAT/100, marza=st.session_state.marza_value/100)
+    table_formatting(temp_data)
+
 
 def create_table_from_marza(zakup_brutto, VAT, marza):
     return ({'zakup netto': round(zakup_brutto / (1 + VAT), 2),
@@ -130,43 +89,11 @@ def create_table_from_marza(zakup_brutto, VAT, marza):
              'cena brutto': round(zakup_brutto / (1 - marza), 2),
              'cena netto': round(zakup_brutto/(1 - marza) / (1 + VAT), 2)})
 
-def print_table_marza():
-    temp_dict = create_table_from_zakup_brutto(zakup_brutto= st.session_state.zakup_brutto_value, VAT=st.session_state.VAT/100, marza=st.session_state.marza_value/100)
-    temp_df = pd.DataFrame.from_dict(temp_dict, orient='index', columns=['Wartości']).transpose()
-    st.data_editor(
-        temp_df,
-        column_order=['zakup netto', 'zakup brutto', 'VAT', 'marża brutto', 'zysk', 'cena netto', 'cena brutto'],
-        column_config={
-            "zakup netto": st.column_config.NumberColumn(
-                "Cena zakupu netto",
-                help="Cena zakupu netto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "zakup brutto": st.column_config.NumberColumn(
-                "Cena zakupu brutto",
-                help="Cena zakupu brutto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "VAT": st.column_config.NumberColumn(
-                "Stawka VAT",
-                help="Stawka podatku VAT wyrażona procentowo",
-                format="%.2f %%"),
-            "marża brutto": st.column_config.NumberColumn(
-                "Stawka marży brutto",
-                help="Marża brutto wyrażona procentowo",
-                format="%.2f %%"),
-            "zysk": st.column_config.NumberColumn(
-                "Zysk netto",
-                help="Zysk wyrażony w złotówkach, stanowiący różnicę między ceną sprzedaży netto a ceną zakupu netto",
-                format="%.2f zł"),
-            "cena netto": st.column_config.NumberColumn(
-                "Cena sprzedaży netto",
-                help="Cena sprzedaży netto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "cena brutto": st.column_config.NumberColumn(
-                "Cena sprzedaży brutto",
-                help="Cena sprzedaży brutto wyrażona w złotówkach",
-                format="%.2f zł"),
-        }
-    )
+
+def print_table_from_marza():
+    temp_data = create_table_from_zakup_brutto(zakup_brutto= st.session_state.zakup_brutto_value, VAT=st.session_state.VAT/100, marza=st.session_state.marza_value/100)
+    table_formatting(temp_data)
+
 
 def create_table_from_cena_brutto(cena_brutto, VAT, marza):
     return ({'zakup netto': round(-(marza*cena_brutto-cena_brutto) / (1+VAT),2),
@@ -177,53 +104,21 @@ def create_table_from_cena_brutto(cena_brutto, VAT, marza):
              'cena brutto': round(cena_brutto, 2),
              'cena netto': round(cena_brutto / (1 + VAT), 2)})
 
-def print_table_cena_brutto():
-    temp_dict = create_table_from_cena_brutto(cena_brutto= st.session_state.cena_brutto_value, VAT=st.session_state.VAT/100, marza=st.session_state.marza_value/100)
-    temp_df = pd.DataFrame.from_dict(temp_dict, orient='index', columns=['Wartości']).transpose()
-    st.data_editor(
-        temp_df,
-        column_order=['zakup netto', 'zakup brutto', 'VAT', 'marża brutto', 'zysk', 'cena netto', 'cena brutto'],
-        column_config={
-            "zakup netto": st.column_config.NumberColumn(
-                "Cena zakupu netto",
-                help="Cena zakupu netto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "zakup brutto": st.column_config.NumberColumn(
-                "Cena zakupu brutto",
-                help="Cena zakupu brutto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "VAT": st.column_config.NumberColumn(
-                "Stawka VAT",
-                help="Stawka podatku VAT wyrażona procentowo",
-                format="%.2f %%"),
-            "marża brutto": st.column_config.NumberColumn(
-                "Stawka marży brutto",
-                help="Marża brutto wyrażona procentowo",
-                format="%.2f %%"),
-            "zysk": st.column_config.NumberColumn(
-                "Zysk netto",
-                help="Zysk wyrażony w złotówkach, stanowiący różnicę między ceną sprzedaży netto a ceną zakupu netto",
-                format="%.2f zł"),
-            "cena netto": st.column_config.NumberColumn(
-                "Cena sprzedaży netto",
-                help="Cena sprzedaży netto wyrażona w złotówkach",
-                format="%.2f zł"),
-            "cena brutto": st.column_config.NumberColumn(
-                "Cena sprzedaży brutto",
-                help="Cena sprzedaży brutto wyrażona w złotówkach",
-                format="%.2f zł"),
-        }
-    )
+
+def print_table_from_cena_brutto():
+    temp_data = create_table_from_cena_brutto(cena_brutto= st.session_state.cena_brutto_value, VAT=st.session_state.VAT/100, marza=st.session_state.marza_value/100)
+    table_formatting(temp_data)
+
 
 VAT_display = st.selectbox("Wybierz stawkę VAT", options=[0., 5., 8., 23.], index=3, key='VAT')
 VAT = VAT_display/100
 
 zakup_brutto, zakup_netto, marza, cena_brutto = st.columns(4)
 with zakup_netto:
-    zakup_netto = st.number_input("Wprowadź cenę zakupu netto", step=0.01, key='zakup_netto_value', on_change=print_table_zakup_netto)
+    zakup_netto = st.number_input("Wprowadź cenę zakupu netto", step=0.01, key='zakup_netto_value', on_change=print_table_from_zakup_netto)
 with zakup_brutto:
-    zakup_brutto = st.number_input("Wprowadź cenę zakupu brutto", step=0.01, key='zakup_brutto_value', on_change=print_table_zakup_brutto)
+    zakup_brutto = st.number_input("Wprowadź cenę zakupu brutto", step=0.01, key='zakup_brutto_value', on_change=print_table_from_zakup_brutto)
 with marza:
-    marza = st.number_input("Wprowadź marżę brutto", step=0.01, value=50., key='marza_value', on_change=print_table_marza)
+    marza = st.number_input("Wprowadź marżę brutto", step=0.01, value=50., key='marza_value', on_change=print_table_from_marza)
 with cena_brutto:
-    cena_brutto = st.number_input("Wprowadź cenę sprzedaży brutto", step=0.01, key='cena_brutto_value', on_change=print_table_cena_brutto)
+    cena_brutto = st.number_input("Wprowadź cenę sprzedaży brutto", step=0.01, key='cena_brutto_value', on_change=print_table_from_cena_brutto)
